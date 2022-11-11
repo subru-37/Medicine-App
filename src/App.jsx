28848cf3ddd0da297a8b526fe3498edea6188d9e
subru-37 <easwaransubramaniOO7@gmail.com>
2022-11-12 02:53:57 +0530

    fixed import error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import './App.css';
 import ResponsiveAppBar from './Components/Navbar/ResponsiveAppBar';
 import MobileAppBar from './Components/Navbar/MobileAppBar';
-import Landing from './pages/Landing/Landing';
+import Landing from './Pages/Landing/Landing';
 export default function App() {
 
   return (
